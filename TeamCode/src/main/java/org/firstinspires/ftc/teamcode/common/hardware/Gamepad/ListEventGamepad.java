3b5d5f0112e0b86b128e7b2d68a74231699a9749
cainiaoyizhou <103909046+cainiaoyizhou@users.noreply.github.com>
2025-08-03 10:25:39 +0800

    完成ListEventGamepad及其配套

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware/Gamepad/ListEventGamepad.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware/Gamepad/ListEventGamepad.java
@@ -2,16 +2,23 @@ package org.firstinspires.ftc.teamcode.common.hardware.Gamepad;
 
 import com.qualcomm.robotcore.hardware.Gamepad;
 
+import org.firstinspires.ftc.teamcode.common.hardware.Gamepad.controllers.Buttons;
+
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 public class ListEventGamepad extends Gamepad {
+    public static boolean PRESS = true;
+    public static boolean RELEASE = false;
+
     private final ScheduledExecutorService pollingExecutor =
             Executors.newSingleThreadScheduledExecutor();
     private final Map<Buttons, CopyOnWriteArrayList<Runnable>>
@@ -31,12 +38,59 @@ public class ListEventGamepad extends Gamepad {
         return Collections.unmodifiableMap(mapToInit);
     }
 
-    public ListEventGamepad() {
-        pollingExecutor.schedule()
+    private static class AsyncRunnable implements Runnable {
+        private final Runnable action;
+        private final Executor executor;
+
+        public AsyncRunnable(Runnable action, Executor executor) {
+            this.action = action;
+            this.executor = executor;
+        }
+
+        @Override
+        public void run() {
+            executor.execute(action);
+        }
     }
 
-    private void pollGamepadState() {
+    private static class AsyncConsumer<T> implements Consumer<T> {
+        private final Consumer<T> action;
+        private final Executor executor;
 
+        public AsyncConsumer(Consumer<T> action, Executor executor) {
+            this.action = action;
+            this.executor = executor;
+        }
+
+        @Override
+        public void accept(T arg) {
+            executor.execute(() -> action.accept(arg));
+        }
+    }
+
+    public ListEventGamepad() {
+        pollingExecutor.schedule(this::pollControllerState, 10L, TimeUnit.MILLISECONDS);
+    }
+
+    public void pollControllerState() {
+        for (Buttons button : Buttons.values()) {
+            if (button.wasJustPressed(this)) {
+                for (Runnable action : Objects.requireNonNull(registeredButtonPress.get(button))) {
+                    action.run();
+                }
+                for (Consumer<Boolean> action : Objects.requireNonNull(registeredButtonChange.get(button))) {
+                    action.accept(PRESS);
+                }
+            }
+            if (button.wasJustReleased(this)) {
+                for (Runnable action : Objects.requireNonNull(registeredButtonRelease.get(button))) {
+                    action.run();
+                }
+                for (Consumer<Boolean> action : Objects.requireNonNull(registeredButtonChange.get(button))) {
+                    action.accept(RELEASE);
+                }
+            }
+        }
     }
 
     public void onButtonPress(Buttons button, Runnable action) {
